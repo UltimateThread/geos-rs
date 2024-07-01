956030f66a37a30d966521e02047f399d817ec21
Aaron Brasington <abrasington0217@gmail.com>
2024-07-01 16:17:04 -0400

    Restructured Test Folders

---
+++ src/tests/algorithm/mod.rs
@@ -0,0 +1,8 @@
+mod angle_tests;
+mod area_test;
+mod cg_algorithms_dd_tests;
+mod distance_tests;
+mod length_tests;
+mod ray_crossing_counter_tests;
+mod point_location_tests;
+mod intersection_tests;
---
+++ src/tests/geom/mod.rs
@@ -0,0 +1,11 @@
+mod coordinate_tests;
+mod coordinate_array_sequences_tests;
+mod coordinate_arrays_tests;
+mod coordinate_list_tests;
+mod envelope_tests;
+mod line_segment_tests;
+mod packed_coordinate_sequences_tests;
+mod multi_point_impl_tests;
+mod intersection_matrix_tests;
+mod triangle_tests;
+mod triangle_cercumcentre_tests;
